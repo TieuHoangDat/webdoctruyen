8f0fdf4b3771c6ed3e7ea8d267e7bed4d7294053
hieucoi020303@gmail.com <hieucoi020303@gmail.com>
2023-09-23 08:07:48 +0700

    Change css

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(NovelGenre)
+admin.site.register(NovelAuthor)
+admin.site.register(Novel)
+admin.site.register(Chapter)
+admin.site.register(Comment)
