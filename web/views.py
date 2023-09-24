1173d30ac4348c6fe4d0689a47a0dd0a786d7442
hieucoi020303@gmail.com <hieucoi020303@gmail.com>
2023-09-24 17:00:58 +0700

    Change css

--- web/views.py
+++ web/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    top_like =  Novel.objects.all().order_by("-total_likes")[15:25]
+    top_like =  Novel.objects.all().order_by("-total_likes")[5:13]
 
     context = {
         "top_like" : top_like,
