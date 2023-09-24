d1aa38069477464f44249218fff39f48e9200337
TieuHoangDat <DatTH.B21CN220@stu.ptit.edu.vn>
2023-09-24 10:51:31 +0700

    sua_dang_ki

--- web/views.py
+++ web/views.py
@@ -22,6 +22,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     context = {'form':form}  
     return render(request, 'register.html', context)
 
